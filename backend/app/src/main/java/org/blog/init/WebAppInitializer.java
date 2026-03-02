a0855137e39ed3d54161d5a3e722629554943ac7
aleksandr kleymenov <akleymenov@fil-it.ru>
2026-03-02 10:30:09 +0300

    Создан базовый каркас приложения

---
+++ backend/app/src/main/java/org/blog/init/WebAppInitializer.java
@@ -0,0 +1,28 @@
+package org.blog.init;
+
+
+import org.blog.WebConfiguration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("=== WebAppInitializer STARTED ==="); // Отладочный вывод
+
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfiguration.class);
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", servlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/"); // Все запросы идут через DispatcherServlet
+    }
+}
